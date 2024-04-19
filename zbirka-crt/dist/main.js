cd82dca503b9ba9a7619a79968ecdaf8be85bfb0
Robin Vikman <robin.alexander.wikman@gmail.com>
2024-04-19 22:11:50 +0200

    test

--- zbirka-crt/dist/main.js
+++ zbirka-crt/dist/main.js
@@ -108,7 +108,7 @@ var jezik = function jezik(sl) {
   slo = sl;
   return slo ? helpTextSlo : helpTextEn;
 };
-var _banner = "\n\xAB UMA MUSK PRESENTS \xBB\n\n\u2592\u2588\u2580\u2580\u2584 \u2580\u2588\u2580 \u2592\u2588\u2591\u2591\u2592\u2588 \u2580\u2588\u2580 \u2592\u2588\u2584\u2591\u2592\u2588 \u2580\u2588\u2580 \u2580\u2580\u2588\u2580\u2580 \u2592\u2588\u2591\u2591\u2592\u2588\n\u2592\u2588\u2591\u2592\u2588 \u2592\u2588\u2591 \u2591\u2592\u2588\u2592\u2588\u2591 \u2592\u2588\u2591 \u2592\u2588\u2592\u2588\u2592\u2588 \u2592\u2588\u2591 \u2591\u2592\u2588\u2591\u2591 \u2592\u2588\u2584\u2584\u2584\u2588\n\u2592\u2588\u2584\u2584\u2580 \u2584\u2588\u2584 \u2591\u2591\u2580\u2584\u2580\u2591 \u2584\u2588\u2584 \u2592\u2588\u2591\u2591\u2580\u2588 \u2584\u2588\u2584 \u2591\u2592\u2588\u2591\u2591 \u2591\u2591\u2592\u2588\u2591\u2591\n\n<img src=\"https://exampl";
+var _banner = "\n\xAB UMA MUSK PRESENTS \xBB\n\n\n<img src=\"https://exampl";
 var helpTextSlo = "\nCommands:\n* find <keyword> - Lists item IDs matching the keywords.\n* item <id> - Displays details about an item.\n* exhibitions [id] - List all exibitions or details of one specified by ID.\n* stats - Displays collection statistics.\n* clear - Clears the screen.\n* SVENSKA - Byt spr\xE5k till Svenska. (OBS: Inmatningar \xE4nnu ej \xF6versatta.)";
 var helpTextEn = "\nCommands:\n* find <keyword> - Lists item IDs matching the keywords.\n* item <id> - Displays details about an item.\n* exhibitions [id] - List all exibitions or details of one specified by ID.\n* stats - Displays collection statistics.\n* clear - Clears the screen.\n* ENGLISH - Switch to English language. (NOTE: Entries not yet translated.)";
 var vec = '';
